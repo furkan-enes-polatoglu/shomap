186c8782685fbc87f6fb5633f9a3f19b78dfff1b
Furkan Enes POLATOĞLU <furkanenes1160@gmail.com>
2023-04-26 18:06:29 +0300

    Update shomap.py

--- shomap.py
+++ shomap.py
@@ -32,16 +32,13 @@ print("\n\n"+colored_info + ' IP: {}'.format(ip))
 
 api = shodan.Shodan(SHODAN_API_KEY)
 
-
 try:
     results = api.host(ip)
 
-    
     for port in results['ports']:
         print("---------------------------------------------------------")
         print(colored_info + ' Port: {}'.format(port))
-        
-        
+
         nm = nmap.PortScanner()
         nm.scan(ip, str(port))
         print(colored_info + " Port is " +  nm[ip]['tcp'][port]['state'])
@@ -53,26 +50,26 @@ try:
             print(colored_err + ' Product: Not found')
         print(colored_info + ' Version: {}'.format(version))
 
-     
-        query = '{} {} exploit-db'.format(product, version)
-        url = 'https://www.google.com/search?q={}'.format(query)
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-        res = requests.get(url, headers=headers)
-        soup = BeautifulSoup(res.text, 'html.parser')
+        try:
+            query = '{} {} exploit-db'.format(product, version)
+            url = 'https://www.google.com/search?q={}'.format(query)
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+            res = requests.get(url, headers=headers)
+            soup = BeautifulSoup(res.text, 'html.parser')
 
-       
-        exploit_db_url = None
-        for link in soup.find_all('a'):
-            href = link.get('href')
-            if href and 'exploit-db.com/exploits' in href:
-                exploit_db_url = href
-                break
+            exploit_db_url = None
+            for link in soup.find_all('a'):
+                href = link.get('href')
+                if href and 'exploit-db.com/exploits' in href:
+                    exploit_db_url = href
+                    break
 
-        
-        if exploit_db_url:
-            print(colored_success + ' Exploit-DB URL: {}'.format(exploit_db_url))
-        else:
-            print('Exploit-DB URL bulunamadı.')
+            if exploit_db_url:
+                print(colored_success + ' Exploit-DB URL: {}'.format(exploit_db_url))
+            else:
+                print('Exploit-DB URL not found.')
+        except requests.exceptions.RequestException as e:
+            print(colored_err + ' Error: {}'.format(e))
 
 except shodan.APIError as e:
-    print('Hata: {}'.format(e))
+    print(colored_err + ' Error: {}'.format(e))
